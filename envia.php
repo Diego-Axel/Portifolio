bf0f169427647e4dfb2000357a25d2083f9eb12d
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-03-02 17:51:11 -0300

    final code

--- envia.php
+++ envia.php
@@ -2,5 +2,20 @@
 
     $nome = addcslashes($_POST['nome']);
     $emaill = addcslashes($_POST['emaill']);
+    $celular = addcslashes($_POST['celular']);
+    $mensagem = addcslashes($_POST['mensagem']);
+
+    $para = "diegoaxelbsr@gmail.com";
+    $assunto = "Coleta de dados - Site_Portifólio";
+
+    $corpo = "Nome: ".$nome."\n"."E-mail ".$emaill."\n"."Celular: ".$celular."\n"."Mensagem: ".$mensagem.
+
+    $cabeca = "From: diego.axel.bernardo.097@ufrn.edu"."\n"."Reply-to: ".$emaill."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo("E-mail enviado com sucesso!");
+    }else{
+        echo("Houve um erro ao enviar o e-mail!");
+    }
 
 ?>
