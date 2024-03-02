c2bb90baa9a8b8a44cb0cd3be00c5b8dd83498dd
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-03-02 16:57:43 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,3 +1,12 @@
-
 var diegoMenu = document.getElementById('diego-menu')
 var menu = document.getElementById('menu-mobile')
+
+
+diegoMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
